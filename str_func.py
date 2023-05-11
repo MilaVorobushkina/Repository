b8d110ab3c65bd1b1f255fd999a03ed699ed823c
Mila <liudika08@mail.ru>
2023-05-11 19:44:31 +0300

    add function title and docs upper

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_str(value):
+    """ Преобразует все буквы в загдавные"""
     return value.upper()
+
+def title_str(value):
+    """ Преобразует первые буквы в словах в заглавные"""
+    return value.title()
